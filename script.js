2a484f6f7a8d5ccc0cf0bbba464f5dd6ad8dae9c
DianaBerte <dianaberte.go@gmail.com>
2023-01-18 15:04:04 +0100

    books are displayed

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const getAllProducts = async (endpoint = "books") => {
     let res = await fetch("https://striveschool-api.herokuapp.com/" + endpoint)
     let books = await res.json()
-    console.log(books)
+    // console.log(books)
+    renderBooks(books)
 }
 getAllProducts()
 
+const renderBooks = function (books) {
+    let containerDiv = document.querySelector(".container")
+    let rowDiv = document.querySelector(".row")
+    let arrayOfBooks = books.map((singleBook) => {
+        return ` 
+        <div class="col-6 d-flex col-lg-3 col-md-4 mt-5 mb-5">
+        <div class="card">
+            <img src="${singleBook.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${singleBook.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button>Add to Cart</button>
+            <button>Skip</button>
+            </div>
+        </div>
+    </div>
+    `
+    }).join("")
+    rowDiv.innerHTML = arrayOfBooks
+}
